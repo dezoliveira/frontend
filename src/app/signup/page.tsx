624937cd12ac9154bb68e487fa503630ff8efa10
Andres Oliveira <andresoliveira@protonmail.com>
2025-02-28 15:10:26 -0300

    create signup page

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,9 +1,59 @@
+import Image from "next/image"
+import Link from "next/link"
+import styles from '../page.module.scss'
+import logoImg from '/public/logo.svg'
+
 export default function Signup() {
 	return (
 		<>
-			<main>
-				<h1>PAGINA CADASTRO</h1>
-			</main>
+			<div className={styles.containerCenter}>
+				<Image
+					src={logoImg}
+					alt="Logo da pizzaria"
+				/>
+
+				<section className={styles.login}>
+					<h1>Criando a sua conta</h1>
+					<form>
+						{/* nome */}
+						<input
+            	type='text'
+            	required
+            	name="name"
+            	placeholder="Digite seu nome..."
+            	className={styles.input}
+          	/>
+
+						{/* email */}
+						<input
+							type='email'
+							required
+							name="email"
+							placeholder="Digite seu email..."
+							className={styles.input}
+						/>
+
+						{/* password */}
+						<input
+							type="password"
+							required
+							name="password"
+							placeholder="*************"
+							className={styles.input}
+						/>
+
+						<button type="submit" className={styles.button}>
+							Cadastrar
+						</button>
+					</form>
+
+					<Link href="/" className={styles.text}>
+						Já possui uma conta ? Faça Login
+					</Link>
+
+				</section>
+
+			</div>
 		</>
 	)
 }
