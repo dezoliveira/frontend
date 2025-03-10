276b6f591d9c07d0c1a1859202ab2bdcaf57929c
Andres Oliveira <andresoliveira@protonmail.com>
2025-03-10 16:24:34 -0300

    categories register

--- src/app/dashboard/category/page.tsx
+++ src/app/dashboard/category/page.tsx
@@ -1,10 +1,36 @@
 import { Button } from '@/app/dashboard/components/button'
 import styles from './styles.module.scss'
+import { api } from '@/services/api'
+
+import { getCookieServer } from '@/lib/cookieServer'
+import { redirect } from 'next/navigation'
 
 export default function Category() {
   
-  async function handleRegisterCategory() {
+  async function handleRegisterCategory(formData: FormData) {
     "use server"
+
+    const name = formData.get("name")
+
+    if (name === "") return
+
+    const data = {
+      name: name
+    }
+
+    const token = await getCookieServer()
+
+    await api.post("/category", data, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+    .catch((err) => {
+      console.log(err)
+      return
+    })
+
+    redirect("/dashboard")
   }
   
   return(
