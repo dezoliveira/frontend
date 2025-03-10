0fe4c7afb2ae8c56d5b926557a39671dfb0115bd
Andres Oliveira <andresoliveira@protonmail.com>
2025-03-10 11:35:51 -0300

    add category page

--- src/app/dashboard/category/page.tsx
+++ src/app/dashboard/category/page.tsx
@@ -1,7 +1,24 @@
+import { Button } from '@/app/dashboard/components/button'
+import styles from './styles.module.scss'
+
 export default function Category() {
   return(
-    <main>
-      <h1>Página Categorias</h1>
+    <main className={styles.container}>
+      <h1>Nova Categoria</h1>
+
+      <form
+        className={styles.form}
+      >
+        <input
+          type="text"
+          name="name"
+          placeholder="Nome da categoria, ex: Pizzas"
+          required
+          className={styles.input}
+        />
+      </form>
+
+      <Button name="Cadastrar"/>
     </main>
   )
 }
