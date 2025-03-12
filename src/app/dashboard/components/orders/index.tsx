eed66873899cef2d4b3705d697b422ff29352447
Andres Oliveira <andresoliveira@protonmail.com>
2025-03-12 16:39:05 -0300

    refresh button and no orders message

--- src/app/dashboard/components/orders/index.tsx
+++ src/app/dashboard/components/orders/index.tsx
@@ -6,6 +6,8 @@ import { RefreshCcw } from 'lucide-react'
 import { OrderProps } from '@/lib/order.type'
 import { ModalOrder } from '@/app/dashboard/components/modal'
 import { OrderContext } from '@/providers/order'
+import { useRouter } from 'next/navigation'
+import { toast } from 'sonner'
 
 interface Props {
   orders: OrderProps[]
@@ -13,10 +15,16 @@ interface Props {
 
 export function Orders({ orders }: Props) {
   const { isOpen, onRequestOpen } = use(OrderContext)
+  const router = useRouter()
   
   async function handleDetailOrder(order_id: string) {
     await onRequestOpen(order_id)
-  }  
+  } 
+
+  function handleRefresh() {
+    router.refresh()
+    toast.success("Pedidos atualizados com sucesso!")
+  }
 
   return(
     <>
@@ -24,12 +32,17 @@ export function Orders({ orders }: Props) {
 
         <section className={styles.containerHeader}>
           <h1>Últimos pedidos</h1>
-          <button>
+          <button onClick={handleRefresh}>
             <RefreshCcw size={24} color="#3fffa3"/>
           </button>
         </section>
 
         <section className={styles.listOrders}>
+          {orders.length === 0 && (
+            <span className={styles.emptyItem}>
+              Nenhum pedido aberto no momento...
+            </span>
+          )}
           {orders.map(order => (
             <button
               key={order.id}
